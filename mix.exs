b99cc376d191b0826324e619d799b4982f59dab1
Jeremy Knope <me@jeremyknope.com>
2025-12-30 11:19:51 -0500

    chore: use mise, default to postgres for dev

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule FuzzyRss.MixProject do
     [
       app: :fuzzy_rss,
       version: "0.1.0",
-      elixir: "~> 1.15",
+      elixir: "~> 1.19",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
